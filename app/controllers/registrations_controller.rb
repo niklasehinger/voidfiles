9a70995a595fafa74d37766951bf54070d8234cd
niklasehinger <info@niklasehinger.de>
2026-02-24 15:06:22 +0100

    Fix trailing whitespace in registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@
 class RegistrationsController < Devise::RegistrationsController
   def update
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    
+
     resource_updated = update_resource(resource, account_update_params)
     yield resource if block_given?
 
